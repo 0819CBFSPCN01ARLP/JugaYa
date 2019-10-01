bcadbd12ce03e40e4dfec4970dd3115b24741eaa
brunomalpeli <53841931+brunomalpeli@users.noreply.github.com>
2019-10-01 18:10:41 -0300

    Elimine if que chequeaba si existe el archivo.

--- ProyectV2/Includes/auth.php
+++ ProyectV2/Includes/auth.php
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+<?php
+function validacion()
+
+{
+  $errores=[];
+  if(strlen($_POST["nombre"])==0){
+  $errores[]="Debe indicar un nombre";
+}
+  if(strlen($_POST["apellido"])==0){
+  $errores[]="Debe indicar un apellido";
+}
+  if(strlen($_POST["usuario"])<=3){
+  $errores[]="El usuario debe tener mas de 3 caracteres";
+}
+
+  if(filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)==false){
+  $errores[]="El email no es valido";
+}
+ $extension=pathinfo($_FILES["fotoperfil"]["name"], PATHINFO_EXTENSION);
+  if ($extension !="jpg" && $extension !="jpeg" && $extencion !="png") {
+    $errores[]="El formato de la foto no es valido";
+  }
+
+return $errores;
+}
+
+ ?>
+=======
 <?php
 function validacion(){
   $errores=[];
@@ -81,3 +110,4 @@ function agregarUsuarios($errores){
   }
 } */
 ?>
+>>>>>>> 13b88074a0be6468672031a76a70bdea835c3139
