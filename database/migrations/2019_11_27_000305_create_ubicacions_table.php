af88cc587d97e3e12d66462200794fa60e0fbbfb
Bruno Malpeli <bruno.malpeli@hotmail.com>
2019-11-26 21:18:03 -0300

    modificaciones en clase

---
+++ database/migrations/2019_11_27_000305_create_ubicacions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUbicacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ubicacions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string("direccion");
+            $table->string("nombre");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ubicacions');
+    }
+}
